a05dc48782b2cca7b5510e5ad7577f0fac7fb103
Michael Sachs <micsac@kep.local>
2019-11-26 19:10:44 +0100

    replace missing images from git talk

---
+++ DayOne/say/R/data.R
@@ -0,0 +1,26 @@
+#' First names
+#' 
+#' A vector with four common English first names.
+#' 
+#' @format A character vector of length four. 
+#' @source Synthetic data set
+"names"
+
+
+
+#' R Core members
+#' 
+#' Names of R core members, scraped from the R homepage.
+#' 
+#' @format A data frame with 20 rows and two columns:
+#' \describe{
+#'    \item{First}{First, or given, name of the R core members}
+#'    \item{Last}{Last, or family, name(s)}
+#' }
+#' 
+#' @source \url{https://www.r-project.org/contributors.html}, accessed 2019-11-25
+"corenames"
+
+
+
+
---
+++ docs/DayOne/say.R
@@ -0,0 +1,10 @@
+#' A package for saying things
+#' 
+#' This package allows the user to say hello (for now).
+#' 
+#' @section Future plans:
+#' Many, but currently focusing on good bye.
+#' 
+#' @docType package
+#' @name say
+NULL
